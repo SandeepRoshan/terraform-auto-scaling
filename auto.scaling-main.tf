4a59bf5c0b788e86f98d1378734114dd7e626fe0
SandeepRoshan <115892000+SandeepRoshan@users.noreply.github.com>
2024-08-19 16:35:37 +0530

    created Auto Scaling

---
+++ auto.scaling-main.tf
@@ -0,0 +1,90 @@
+
+/*
+ | --
+ | -- When the clustered ec2 instances are reaching the end of their
+ | -- tether this auto scaling group (and launch configuration) sees
+ | -- to it that more are automatically provisioned.
+ | --
+ | -- Dynamic tags are used here because the auto-scaling group will
+ | -- be responsible for placing tags on the provisioned ec2 intances.
+ | --
+*/
+resource aws_autoscaling_group this {
+
+    name                 = "${ var.in_ecosystem }-asg-${ var.in_timestamp }"
+    vpc_zone_identifier  = var.in_subnet_ids
+    launch_configuration = aws_launch_configuration.this.name
+    min_size             = var.in_minimum_instances
+    max_size             = var.in_maximum_instances
+    desired_capacity     = var.in_desired_instances
+
+    tag {
+
+        key                 = "Name"
+        value               = "${ var.in_ecosystem }-worker-${ var.in_timestamp }"
+        propagate_at_launch = true
+    }
+
+    tag {
+
+        key                 = "Desc"
+        value               = "This auto scaled ec2 instance ( for ${ var.in_ecosystem } ) ${ var.in_description }"
+        propagate_at_launch = true
+    }
+
+    dynamic tag {
+
+        for_each = var.in_mandated_tags
+        content {
+            key   = tag.key
+            value = tag.value
+            propagate_at_launch = true
+        }
+    }
+}
+
+
+/*
+ | --
+ | -- This launch configuration ensures that
+ | --
+ | --   a) a given AMI is used to raise the instances
+ | --   b) the instance type is of a particular ilk
+ | --   c) the ec2 instance adopts a role profile carrying access policies
+ | --   d) each instance is plied with a public SSH key
+ | --   e) the security group for each instance is the one stated
+ | --   f) the user data scripts are run after the instance boots up
+ | --
+*/
+resource aws_launch_configuration this {
+
+    name_prefix          = "${ var.in_ecosystem }-launch-config-${ var.in_timestamp }"
+    image_id             = var.in_ami_id
+    instance_type        = var.in_instance_type
+    iam_instance_profile = var.in_instance_profile_id
+    key_name             = aws_key_pair.ssh.id
+    security_groups      = [ var.in_security_group_id ]
+    user_data            = var.in_user_data_script
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+
+/*
+ | --
+ | -- The ec2 instances fashioned by the launch configuration are
+ | -- plied with this public key to enable troubleshooters to go in
+ | -- and diagnose issues.
+ | --
+ | -- Best practise is to block ssh on production systems but if
+ | -- and when you need to get out of jail free, you place a security
+ | -- group rule allowing it and proceed to perform diagnosis.
+ | --
+*/
+resource aws_key_pair ssh {
+    key_name = "key-4-${ var.in_ecosystem }-${ var.in_timestamp }"
+    public_key = var.in_ssh_public_key
+}
---
+++ auto.scaling-variables.tf
@@ -0,0 +1,157 @@
+
+### ######### ###
+### in_ami_id ###
+### ######### ###
+
+variable in_ami_id {
+
+    description = "The ID of the amazon machine image to use as a template for raising the instances."
+    type        = string
+}
+
+
+### ################ ###
+### in_instance_type ###
+### ################ ###
+
+variable in_instance_type {
+
+    description = "The intance type that defaults with 2 virtual CPUs, 4Gig of RAM and 24 credits an hour."
+    default     = "t2.medium"
+    type        = string
+}
+
+
+### ###################### ###
+### in_instance_profile_id ###
+### ###################### ###
+
+variable in_instance_profile_id {
+
+    description = "The ID of the profile giving the ec2 access to other AWS resources like S3, ECS and RDS."
+    type        = string
+}
+
+
+### ############################## ###
+### in_ssh_public_key ###
+### ############################## ###
+
+variable in_ssh_public_key {
+
+    description = "The public key to ply into the ec2 instances raised by this launch configuration."
+    type        = string
+}
+
+
+### ################################ ###
+### in_user_data_script ###
+### ################################ ###
+
+variable in_user_data_script {
+
+    description = "The user data compliant script to run within the ec2 instance right after it is created."
+    default     = ""
+    type        = string
+}
+
+
+### #################### ###
+### in_minimum_instances ###
+### #################### ###
+
+variable in_minimum_instances {
+
+    description = "The minimum number of instance to always have ready to handle requests defaulted at 1."
+    type        = number
+    default     = 3
+}
+
+
+### #################### ###
+### in_maximum_instances ###
+### #################### ###
+
+variable in_maximum_instances {
+
+    description = "The maximum number of instance that this auto scaling group will raise to handle your workload."
+    type        = number
+    default     = 7
+}
+
+
+### #################### ###
+### in_desired_instances ###
+### #################### ###
+
+variable in_desired_instances {
+
+    description = "The goldilocks instance count when your request levels are not to hot and not too cold."
+    type        = number
+    default     = 4
+}
+
+
+### #################### ###
+### in_security_group_id ###
+### #################### ###
+
+variable in_security_group_id {
+    description = "The ID of the security group that constrains the traffic to and from the raised ec2 instances."
+    type        = string
+}
+
+
+### ########################## ###
+### in_subnet_ids ###
+### ########################## ###
+
+variable in_subnet_ids {
+    description = "IDs of subnets the network interfaces are attached to."
+    type = list
+}
+
+
+### ################ ###
+### in_mandated_tags ###
+### ################ ###
+
+variable in_mandated_tags {
+
+    description = "Optional tags unless your organization mandates that a set of given tags must be set."
+    type        = map
+    default     = { }
+}
+
+
+### ############ ###
+### in_ecosystem ###
+### ############ ###
+
+variable in_ecosystem {
+    description = "Creational stamp binding all infrastructure components created on behalf of this ecosystem instance."
+    default     = "auto-scaling"
+    type        = string
+}
+
+
+### ############ ###
+### in_timestamp ###
+### ############ ###
+
+variable in_timestamp {
+    description = "A timestamp for resource tags in the format ymmdd-hhmm like 80911-1435"
+    default     = "TIMESTAMP"
+    type        = string
+}
+
+
+### ############## ###
+### in_description ###
+### ############## ###
+
+variable in_description {
+    description = "Ubiquitous note detailing who, when, where and why for every infrastructure component."
+    default     = "within the AWS cloud at this point in time."
+    type        = string
+}
